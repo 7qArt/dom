7a46d63d1412d5807d7b3926c302bf7b88bfd6cb
Stile <stilenxx@gmail.com>
2022-07-22 16:24:37 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,15 @@
+<html>
+<body>
+<div>Пользователи:</div>
+<ul>
+    <li>Джон</li>
+    <li>Пит</li>
+</ul>
+
+<script>for (let hello of (document.body.children)</script>
+</body>
+</html>
+
+alert(hello);
+alert(document.body.children);
+alert (tbody.rows[1].cells[1]); 
